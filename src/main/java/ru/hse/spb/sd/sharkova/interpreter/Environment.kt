b671cf6aca5616cdb8a484cc3f30340b23a58c35
VadimFarutin <vfarut@gmail.com>
2019-03-29 12:40:49 +0300

    Added docs for Environment

--- src/main/java/ru/hse/spb/sd/sharkova/interpreter/Environment.kt
+++ src/main/java/ru/hse/spb/sd/sharkova/interpreter/Environment.kt
@@ -5,11 +5,22 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
+/**
+ * Environment represents system state and provides methods to interact with it.
+ */
 object Environment {
     private var currentDirectory = Paths.get(".").toAbsolutePath().normalize()
 
+    /**
+     * Getter for a working directory.
+     * @return current directory
+     */
     fun getCurrentDirectory(): Path = currentDirectory
 
+    /**
+     * Sets a new working directory, if exists.
+     * @param directory path to a new directory
+     */
     fun setCurrentDirectory(directory: String) {
         val newDirectory = currentDirectory.resolve(directory)
 
@@ -20,5 +31,10 @@ object Environment {
         currentDirectory = newDirectory.toAbsolutePath().normalize()
     }
 
+    /**
+     * Getter for a file in or outside working directory.
+     * @param path path to the file
+     * @return required file
+     */
     fun getFile(path: String): File = currentDirectory.resolve(path).toFile()
 }
